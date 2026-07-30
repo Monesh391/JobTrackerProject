60a1cd898ad64ae1fd098a6f429c6b943b20894a
Monesh <hawkmone666@gmail.com>
2026-07-30 21:07:28 +0530

    Update urls.py

--- jobtracker/urls.py
+++ jobtracker/urls.py
@@ -20,6 +20,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 
 urlpatterns = [
@@ -27,4 +32,7 @@ urlpatterns = [
     path('api/', include('tracker.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
